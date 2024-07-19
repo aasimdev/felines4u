d0df8c9e3ba0164eeac32074475851c54bb7636a
Umar <umar.azam@iitsnl.com>
2024-07-20 02:39:54 +0500

    Home Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {
       fontSize: {
         'responsive': 'calc(1.3rem + .6vw)',
+        'responsive-h1': 'calc(1.475rem + 2.7vw)',
+        'responsive-h2': 'calc(1.325rem + .9vw)',
+        
       },
     },
   },
